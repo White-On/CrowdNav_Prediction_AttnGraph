1ea284acae470ee70efe2ac8998382990ac2a3e2
White-On <jules.cassan@hotmail.com>
2024-07-09 11:57:58 +0200

    add PPO stable baseline

--- main_PPO_stable_baseline.py
+++ main_PPO_stable_baseline.py
@@ -1,11 +1,5 @@
-import gymnasium as gym
-import numpy as np
-
-
-from stable_baselines3 import DDPG
-from stable_baselines3.common.noise import (
-    NormalActionNoise,
-)
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
 
 from gym_file.envs.crowd_sim_car import CrowdSimCar
 from gym_file.envs.crowd_sim_car_simple_obs import CrowdSimCarSimpleObs
@@ -14,47 +8,40 @@ from logger import logging_setup
 
 
 def main():
-    logging_setup("DDPG_evaluation.log", level=logging.INFO)
+    logging_setup("PPO_evaluation.log", level=logging.INFO)
     episode_time = 500
     eval = False
-    total_timesteps = 500_000
+    total_timesteps = 50_000
     save_every_n_timesteps = 10_000
     nb_learnging_cycles = total_timesteps // save_every_n_timesteps
-    model_file = "ddpg_CrowdSimCar"
+    model_file = "ppo_CrowdSimCar"
 
-    # logging.info(gym.envs.registry.keys())
-    env = gym.make(
-        "CrowdSimCar-v0",
-        render_mode="human",
-        episode_time=episode_time,
-        nb_pedestrians=10,
-        disable_env_checker=True,
-        load_scenario=None,
-    )
+    # Parallel environments
+    env_kwargs = {
+        "render_mode": "human",
+        "episode_time": episode_time,
+        "nb_pedestrians": 10,
+        "disable_env_checker": True,
+        "load_scenario": None,
+    }
+    vec_env = make_vec_env("CrowdSimCar-v0", n_envs=1, env_kwargs=env_kwargs)
 
-    # The noise objects for DDPG
-    n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(
-        mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions)
-    )
-
-    model = DDPG(
+    model = PPO(
         "MultiInputPolicy",
-        env,
-        action_noise=action_noise,
+        vec_env,
         verbose=1,
         tensorboard_log="runs",
     )
-    if eval:
 
+    if eval:
         vec_env = model.get_env()
-        model = DDPG.load(model_file)
+        model = PPO.load(model_file)
         obs = vec_env.reset()
 
         for _ in range(episode_time):
             action, _states = model.predict(obs)
             obs, rewards, dones, info = vec_env.step(action)
-            env.render()
+            vec_env.render()
 
         return
 
@@ -67,15 +54,14 @@ def main():
         )
         model.save(model_file)
 
-    vec_env = model.get_env()
-    model = DDPG.load(model_file)
+    model = PPO.load(model_file)
 
     obs = vec_env.reset()
 
     for _ in range(episode_time):
         action, _states = model.predict(obs)
         obs, rewards, dones, info = vec_env.step(action)
-        env.render()
+        vec_env.render()
 
 
 if __name__ == "__main__":
